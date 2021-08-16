33320b0a92f9833bf0994e5ba0464ffd6cb7a712
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2021-08-16 20:35:21 +0200

    parse dipole, detect steps

---
+++ gaussian_output_tools/blocks/dipole.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass
+from decimal import Decimal
+from typing import Iterator, Optional, List
+
+import regex as re
+
+from . import Match
+
+DIPOLE_MATCH = re.compile(
+    r"""
+(
+  \ Dipole\ orientation:\n
+  (?:\s* (?P<oanr>\d+) (?:\s+ (?P<oval>\S+)){3} \n)+
+  \n
+)?
+\ Electric\ dipole\ moment\ \((?P<orientation>\w+)\ orientation\):\n
+(?:.+\n){2}
+(?:\s* (?P<direction>\w+) (?:\s+ (?P<val>\S+)){3} \n)+
+\n
+\ Dipole\ polarizability,\ (?P<spin>\w+)\ .+\n
+(?:.+\n){3}
+(?:\s* (?P<pdirection>\w+) (?:\s+ (?P<pval>\S+)){3} \n)+
+""",
+    re.VERBOSE | re.MULTILINE,
+)
+
+@dataclass
+class AtomCoords:
+    nr: int
+    x: Decimal
+    y: Decimal
+    z: Decimal
+
+
+@dataclass
+class Moment:
+    """Electric dipole moment in a.u."""
+    tot: Decimal
+    x: Decimal
+    y: Decimal
+    z: Decimal
+
+@dataclass
+class Polarizability:
+    """Dipole polarizability in a.u."""
+    spin: str
+    iso: Decimal
+    aniso: Decimal
+    xx: Decimal
+    yx: Decimal
+    yy: Decimal
+    xz: Decimal
+    zy: Decimal
+    zz: Decimal
+
+
+@dataclass
+class Dipole:
+    orientation: str
+    atom_orientation: Optional[List[AtomCoords]]
+    moment: Moment
+    polarizability: Polarizability
+
+
+def match_dipole(content: str) -> Iterator[Dipole]:
+    for match in DIPOLE_MATCH.finditer(content):
+
+        if match.captures("oval"):
+            coord_len = len(match.captures("oval"))
+            atom_orientation = [AtomCoords(int(oanr), *(Decimal(v) for v in oval)) for oanr, oval in zip(match.captures("oanr"), (match.captures("oval")[idx:idx+3] for idx in range(0, coord_len, 3)))]
+        else:
+            atom_orientation = None
+
+        moment = Moment(*(Decimal(v.replace("D", "E")) for v in match.captures("val")[::3]))
+        polarizability = Polarizability(match["spin"], *(Decimal(val.replace("D", "E")) for val in match.captures("pval")[::3]))
+
+        yield Match(
+            data=Dipole(match["orientation"], atom_orientation, moment, polarizability),
+            spans=match.spans(),
+        )
--- gaussian_output_tools/cli.py
+++ gaussian_output_tools/cli.py
@@ -1,10 +1,15 @@
 from itertools import chain
+import re
 
 import click
 
 from .blocks import merged_spans
 from .blocks.moments import match_moments
 from .blocks.parameters import match_parameters
+from .blocks.dipole import match_dipole
+
+
+END_MATCH = re.compile(r"^ Normal termination .+\n", re.MULTILINE)
 
 
 @click.command()
@@ -19,30 +24,39 @@ from .blocks.parameters import match_parameters
 def g16parse(fhandle, color):
     """Parse the Gaussian output FILE and return a structured output"""
 
-    spans = []
-
     content = fhandle.read()
-
-    for match in chain(match_moments(content), match_parameters(content)):
-        spans += match.spans
-
-    spans = merged_spans(spans)
-
+    step_contents = []
     ptr = 0
-    for start, end in spans:
+
+    for match in END_MATCH.finditer(content):
+        # the end of this match also marks the end of this
+        step_contents.append(content[ptr:match.span(0)[1]])
+        ptr = match.span(0)[1]
+
+    for stepnr, content in enumerate(step_contents, start=1):
+        spans = []
+        print(f"\nSTEP: {stepnr}\n")
+
+        for match in chain(match_moments(content), match_parameters(content), match_dipole(content)):
+            spans += match.spans
+
+        spans = merged_spans(spans)
+
+        ptr = 0
+        for start, end in spans:
+            click.secho(
+                content[ptr:start],
+                nl=False,
+                dim=True,
+                color=None if color == "auto" else True,
+            )
+            click.secho(
+                content[start:end],
+                nl=False,
+                bold=True,
+                color=None if color == "auto" else True,
+            )
+            ptr = end
         click.secho(
-            content[ptr:start],
-            nl=False,
-            dim=True,
-            color=None if color == "auto" else True,
+            content[ptr:], nl=False, dim=True, color=None if color == "auto" else True
         )
-        click.secho(
-            content[start:end],
-            nl=False,
-            bold=True,
-            color=None if color == "auto" else True,
-        )
-        ptr = end
-    click.secho(
-        content[ptr:], nl=False, dim=True, color=None if color == "auto" else True
-    )
