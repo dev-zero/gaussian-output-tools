b0e03cb5f25269d4bd47873d4ec10af84e5e9f50
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2021-08-18 16:24:18 +0200

    separate "datatree building" and output

--- gaussian_output_tools/cli.py
+++ gaussian_output_tools/cli.py
@@ -1,18 +1,9 @@
-import re
-from itertools import chain
-
 import click
 from rich.console import Console
 from rich.highlighter import RegexHighlighter, ReprHighlighter
 
 from .blocks import merged_spans
-from .blocks.dipole import match_dipole
-from .blocks.frequencies import match_frequencies
-from .blocks.moments import match_moments
-from .blocks.parameters import match_parameters
-from .blocks.scf import match_scf
-
-STEP_MATCH = re.compile(r"^ -+\n #(?P<settings>.+?)\n -+\n", re.MULTILINE | re.DOTALL)
+from .parser import parse_iter
 
 
 class FortranNumberHighlighter(RegexHighlighter):
@@ -49,32 +40,13 @@ def g16parse(fhandle, color, oformat):
 
     content = fhandle.read()
 
-    step_configs = []
-    step_starts = []
-    step_ends = []
     spans = []
 
-    for match in STEP_MATCH.finditer(content):
-        step_configs.append(match["settings"].replace("\n ", ""))
-        step_ends.append(match.span()[0])
-        step_starts.append(match.span()[1] + 1)
-        spans.append(match.span())
-    del step_ends[0]
-    step_ends.append(len(content))
-
-    for step_idx, (step_config, step_start, step_end) in enumerate(
-        zip(step_configs, step_starts, step_ends)
-    ):
+    for step_idx, step in enumerate(parse_iter(content)):
         if oformat == "objects":
-            console.rule(f"[bold red]STEP {step_idx}: {step_config}")
+            console.rule(f"[bold red]STEP {step_idx}: {step.settings}")
 
-        for match in chain(
-            match_scf(content, step_start, step_end),
-            match_moments(content, step_start, step_end),
-            match_parameters(content, step_start, step_end),
-            match_dipole(content, step_start, step_end),
-            match_frequencies(content, step_start, step_end),
-        ):
+        for match in step.data:
             spans += match.spans
             if oformat == "objects":
                 console.print(match.data)
---
+++ gaussian_output_tools/parser.py
@@ -0,0 +1,50 @@
+import re
+from dataclasses import dataclass
+from itertools import chain
+from typing import Iterator, List, Union
+
+from .blocks import Match
+from .blocks.dipole import match_dipole
+from .blocks.frequencies import match_frequencies
+from .blocks.moments import match_moments
+from .blocks.parameters import match_parameters
+from .blocks.scf import match_scf
+
+STEP_MATCH = re.compile(r"^ -+\n #(?P<settings>.+?)\n -+\n", re.MULTILINE | re.DOTALL)
+
+
+@dataclass
+class Step:
+    settings: str
+    data: Union[List[Match], Iterator[Match]]
+
+
+def parse_iter(content: str) -> Iterator[Step]:
+    step_configs = []
+    step_starts = []
+    step_ends = []
+    spans = []
+
+    for match in STEP_MATCH.finditer(content):
+        step_configs.append(match["settings"].replace("\n ", ""))
+        step_ends.append(match.span()[0])
+        step_starts.append(match.span()[1] + 1)
+        spans.append(match.span())
+    del step_ends[0]
+    step_ends.append(len(content))
+
+    for step_config, step_start, step_end in zip(step_configs, step_starts, step_ends):
+        yield Step(
+            step_config,
+            chain(
+                match_scf(content, step_start, step_end),
+                match_moments(content, step_start, step_end),
+                match_parameters(content, step_start, step_end),
+                match_dipole(content, step_start, step_end),
+                match_frequencies(content, step_start, step_end),
+            ),
+        )
+
+
+def parse_all(content: str) -> List[Step]:
+    return [Step(step.settings, list(step.data)) for step in parse_iter(content)]
